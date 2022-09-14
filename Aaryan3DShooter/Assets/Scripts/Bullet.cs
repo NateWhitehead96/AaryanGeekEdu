e1eb7935a385363910d1de40b4c1022cef5731be
NateWhitehead96 <50887818+NateWhitehead96@users.noreply.github.com>
2022-09-14 19:08:08 -0400

    shooting bullets

---
+++ Aaryan3DShooter/Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 5); // when the bullet is shot, after 5 seconds it will disappear
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) // when the bullets hit the ground, also destroy
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Aaryan3DShooter/Assets/Scripts/GunScript.cs
+++ Aaryan3DShooter/Assets/Scripts/GunScript.cs
@@ -6,6 +6,9 @@ public class GunScript : MonoBehaviour
 {
     public float xRotation; // rotation on x axis
     public float rotationSpeed; // how fast we look up and down
+    public GameObject bullet; // access to the bullet prefab
+    public Transform shootPoint; // where the bullet spawns
+    public float shootForce; // how fast the bullet can travel
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +20,34 @@ public class GunScript : MonoBehaviour
     void Update()
     {
         RotateGun(); // rotate up and down
+        Shoot(); // pow pow blam blam
     }
 
     public void RotateGun()
     {
         float mouseY = Input.GetAxisRaw("Mouse Y"); // get the mouse y position
         xRotation = mouseY * rotationSpeed * Time.deltaTime; // set our x rotation
+        if(transform.rotation.eulerAngles.x >= 80) // when the gun is at 80 degrees
+        {
+            //xRotation = 80;
+            transform.rotation = Quaternion.Euler(80, 0, 0); // keep it at 80 degrees on the x axis
+        }
+        if(transform.rotation.eulerAngles.x <= -80)
+        {
+            //xRotation = -80;
+            transform.rotation = Quaternion.Euler(-80, 0, 0); // keep it at 80 degrees on the x axis
+        }
         Vector3 gunRotation = transform.rotation.eulerAngles; // store the guns rotations
         gunRotation.x -= xRotation; // set the rotations
         transform.rotation = Quaternion.Euler(gunRotation); // apply to our gun
+    }
+
+    public void Shoot()
+    {
+        if (Input.GetMouseButtonDown(0)) // when we hit the left mouse button
+        {
+            GameObject newBullet = Instantiate(bullet, shootPoint.position, shootPoint.rotation); // spawn the bullet and store it in newBullet
+            newBullet.GetComponent<Rigidbody>().AddForce(shootPoint.forward * shootForce, ForceMode.Impulse); // add force to bullet
+        }
     }
 }
